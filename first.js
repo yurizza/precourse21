1aa861399ebed0ee9cbeeee3b4049c71186ba775
cici yuriza <ciciyuriza999@gmail.com>
2023-06-19 11:26:32 +0700

    first commit

---
+++ first.js
@@ -0,0 +1,12 @@
+const nilai = [100,200]
+if (nilai[0] === 100){
+    nilai[0] = 90
+    console.log("aku")
+    console.log(nilai)
+}
+else 
+    console.log("kamu")
+
+nilai.forEach(nilai => {
+    console.log(nilai)
+});
